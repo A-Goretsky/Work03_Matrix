39ff93d3bdf16468f783a3bfc71b441c998b60b8
Anton <anton.goretsky@gmail.com>
2017-03-01 07:45:25 -0500

    all complete

--- main.c
+++ main.c
@@ -11,8 +11,103 @@ int main() {
 
   screen s;
   struct matrix *edges;
+  struct matrix *identityTest;
 
+  struct matrix *test1, *test2, *test3;
+
+  identityTest = new_matrix(3, 3);
   edges = new_matrix(4, 4);
+  test1 = new_matrix(4, 4);
+  test2 = new_matrix(4, 7);
+  test3 = new_matrix(4, 4);
+  //printf("~~~Testing print_matrix~~~\n");
+  //print_matrix(edges);
+  //printf("\n");
+  printf("~~~Testing ident~~~\n");
+  ident(edges);
+  print_matrix(edges);
+  printf("\n");
+  printf("~~~Testing ident 2~~~\n");
+  identityTest = new_matrix(3, 3);
+  ident(identityTest);
+  print_matrix(identityTest);
+  printf("\n");
+  printf("~~~Testing scalar_mult~~~\n");
+  printf("Populating Random Matrix:\n");
+  //populating random matrix
+  int x, y;
+  int num = 0;
+  for (x = 0; x < test1->rows; x++) {
+      for (y = 0; y < test1->cols; y++) {
+          num = rand() % 100;
+          test1->m[x][y] = num;
+      }
+  }
+  print_matrix(test1);
+  printf("\n");
 
-  free_matrix( edges );
-}  
+  //testing scalar_mult;
+  printf("Multiply matrix by 4\n");
+  scalar_mult(4, test1);
+  print_matrix(test1);
+  printf("\n");
+
+  printf("Multiply matrix by identity matrix\n");
+  matrix_mult(test1, edges);
+  print_matrix(edges);
+  printf("\n");
+
+  printf("Populating Second Random Matrix\n");
+  int x1;
+  int y1;
+  int num1;
+  for (x1 = 0; x1 < test2->rows; x1++) {
+      for (y1 = 0; y1 < test2->cols; y1++) {
+          num1 = rand() % 100;
+          test2->m[x1][y1] = num1;
+      }
+  }
+
+  print_matrix(test2);
+  printf("\n");
+
+  printf("Product of random matrices:\n");
+  matrix_mult(test1, test2);
+  print_matrix(test2);
+
+  //DRAWING TIME
+  color c;
+  c.red = 106;
+  c.green = 90;
+  c.blue = 205;
+
+  color c1;
+  c1.red = 250;
+  c1.green = 40;
+  c1.blue = 50;
+  clear_screen(s);
+
+  printf("\n");
+  struct matrix *drawing = new_matrix(4, 4);
+  drawing->lastcol = 0;
+  struct matrix *drawing1 = new_matrix(4, 4);
+  drawing1->lastcol = 0;
+  for (int i = 0; i < 25; i++) {
+      add_edge(drawing, 0, 0, 0, XRES, 20 * i, 0);
+      //add_edge(p2, XRES, YRES-20*i, 0, 20*i, YRES, 0);
+  }
+  for (int i = 0; i < 25; i++) {
+      add_edge(drawing1, XRES, YRES, 0, 0, 20 * i, 0);
+      //add_edge(p2, XRES, YRES-20*i, 0, 20*i, YRES, 0);
+  }
+  //add_edge(drawing1, 0, 500, 0, 500, 0, 0);
+  draw_lines(drawing1, s, c1);
+  //add_edge(drawing, 0, 0, 0, 150, 150, 200);
+  print_matrix(drawing);
+  draw_lines(drawing, s, c);
+  display(s);
+  save_extension(s, "image.png");
+
+  //free_matrix(drawing);
+  free_matrix(identityTest);
+}
--- matrix.c
+++ matrix.c
@@ -6,43 +6,89 @@
 
 
 /*-------------- void print_matrix() --------------
-Inputs:  struct matrix *m 
-Returns: 
+Inputs:  struct matrix *m
+Returns:
 
 print the matrix
 */
 void print_matrix(struct matrix *m) {
+    int x;
+    int y;
+    for (x = 0; x < m->rows; x++) {
+        printf("|");
+        for (y = 0; y < m->cols; y++) {
+            double temp = m->m[x][y];
+            printf(" %.2lf ", temp);
+        }
+        printf("|\n");
+    }
 }
 
 /*-------------- void ident() --------------
 Inputs:  struct matrix *m <-- assumes m is a square matrix
-Returns: 
+Returns:
 
 turns m in to an identity matrix
 */
 void ident(struct matrix *m) {
+    int x;
+    int y;
+    for (x = 0; x < m->rows; x++) {
+        for (y = 0; y < m->cols; y++) {
+            if (y == x) {
+                m->m[x][y] = 1;
+            }
+            else {
+                m->m[x][y] = 0;
+            }
+        }
+    }
 }
 
 
 /*-------------- void scalar_mult() --------------
 Inputs:  double x
-         struct matrix *m 
-Returns: 
+         struct matrix *m
+Returns:
 
 multiply each element of m by x
 */
-void scalar_mult(double x, struct matrix *m) {
+void scalar_mult(double num, struct matrix *m) {
+    int x;
+    int y;
+    for (x = 0; x < m->rows; x++) {
+        for (y = 0; y < m->cols; y++) {
+            m->m[x][y] = (num * m->m[x][y]);
+        }
+    }
 }
 
 
 /*-------------- void matrix_mult() --------------
 Inputs:  struct matrix *a
-         struct matrix *b 
-Returns: 
+         struct matrix *b
+Returns:
 
 a*b -> b
 */
 void matrix_mult(struct matrix *a, struct matrix *b) {
+    int x;
+    int y;
+    int z;
+    double num = 0.0;
+    struct matrix *ans;
+    ans = new_matrix(b->rows, b->cols);
+    for (x = 0; x < b->rows; x++) {
+        for (y = 0; y < b->cols; y++) {
+            for (z = 0; z < a->rows; z++) {
+                num += a->m[x][z] * b->m[z][y];
+            }
+            ans->m[x][y] = num;
+        }
+    }
+
+    copy_matrix(ans, b);
+    free_matrix(ans);
 }
 
 
@@ -52,12 +98,12 @@ void matrix_mult(struct matrix *a, struct matrix *b) {
 
 /*-------------- struct matrix *new_matrix() --------------
 Inputs:  int rows
-         int cols 
-Returns: 
+         int cols
+Returns:
 
 Once allocated, access the matrix as follows:
 m->m[r][c]=something;
-if (m->lastcol)... 
+if (m->lastcol)...
 */
 struct matrix *new_matrix(int rows, int cols) {
   double **tmp;
@@ -80,8 +126,8 @@ struct matrix *new_matrix(int rows, int cols) {
 
 
 /*-------------- void free_matrix() --------------
-Inputs:  struct matrix *m 
-Returns: 
+Inputs:  struct matrix *m
+Returns:
 
 1. free individual rows
 2. free array holding row pointers
@@ -100,14 +146,14 @@ void free_matrix(struct matrix *m) {
 
 /*======== void grow_matrix() ==========
 Inputs:  struct matrix *m
-         int newcols 
-Returns: 
+         int newcols
+Returns:
 
 Reallocates the memory for m->m such that it now has
 newcols number of collumns
 ====================*/
 void grow_matrix(struct matrix *m, int newcols) {
-  
+
   int i;
   for (i=0;i<m->rows;i++) {
       m->m[i] = realloc(m->m[i],newcols*sizeof(double));
@@ -118,8 +164,8 @@ void grow_matrix(struct matrix *m, int newcols) {
 
 /*-------------- void copy_matrix() --------------
 Inputs:  struct matrix *a
-         struct matrix *b 
-Returns: 
+         struct matrix *b
+Returns:
 
 copy matrix a to matrix b
 */
@@ -127,8 +173,7 @@ void copy_matrix(struct matrix *a, struct matrix *b) {
 
   int r, c;
 
-  for (r=0; r < a->rows; r++) 
-    for (c=0; c < a->cols; c++)  
-      b->m[r][c] = a->m[r][c];  
+  for (r=0; r < a->rows; r++)
+    for (c=0; c < a->cols; c++)
+      b->m[r][c] = a->m[r][c];
 }
-
